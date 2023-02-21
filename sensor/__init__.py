7ca4475d5486635fc9d2e4dcef54717362289cb8
Mayank Deopa <mayankdeopa999@gmail.com>
2023-02-21 10:48:04 +0530

    updated logger module

---
+++ sensor/__init__.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+
+
+
+
+setup(
+    name="sensor",
+    version="0.0.2",
+    author="Mayank Deopa",
+    author_email="mayankdeopa999@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
